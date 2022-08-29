a84060b72057309da3994acdee1143479245717a
Higashi-Kota <higashi.kota@gri.jp>
2022-08-29 15:46:54 +0900

    add sample

---
+++ react-gsap-scrolltrigger-horizontal-scroll-pin-vite-app/src/components/HorizontalScrollContent.jsx
@@ -0,0 +1,190 @@
+import {css, cx} from '@emotion/css';
+import gsap from 'gsap';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import Lenis from '@studio-freight/lenis';
+import {useState, useMemo, useEffect, useRef} from 'react';
+import {MathUtils} from 'three';
+import {useDebouncedCallback} from 'use-debounce';
+import {BebopLottie} from './BebopLottie';
+
+const HorizontalScrollContent = ({isDebug = false}) => {
+  const scrollerDomRef = useRef(null);
+  const scrollingDomRef = useRef(null);
+  const [progress, setProgress] = useState(0);
+
+  const slides = useMemo(() => {
+    return [
+      {
+        component: () => {
+          return (
+            <div className="flex items-center">
+              <h2 className="text-3xl">Slide 1</h2>
+            </div>
+          );
+        },
+      },
+      {
+        startProgress: 0.25,
+        endProgress: 0.35,
+        component: ({progress, startProgress, endProgress}) => {
+          return (
+            <div className="flex items-center flex-col justify-center">
+              <h2 className="text-3xl">Slide 2</h2>
+              <BebopLottie
+                progress={progress}
+                startProgress={startProgress}
+                endProgress={endProgress}
+              />
+            </div>
+          );
+        },
+      },
+      {
+        component: () => {
+          return (
+            <div className="flex items-center">
+              <h2 className="text-3xl">Slide 3</h2>
+            </div>
+          );
+        },
+      },
+      {
+        startProgress: 0.75,
+        endProgress: 0.85,
+        component: ({progress, startProgress, endProgress}) => {
+          return (
+            <div className="flex items-center flex-col justify-center">
+              <h2 className="text-3xl">Slide 4</h2>
+              <BebopLottie
+                progress={progress}
+                startProgress={startProgress}
+                endProgress={endProgress}
+              />
+            </div>
+          );
+        },
+      },
+    ];
+  }, []);
+
+  const handleResize = useDebouncedCallback((e) => {
+    ScrollTrigger.getById('pc-trigger-scroll')?.kill();
+    setUpScrollTrigger();
+  }, 500);
+
+  const setUpScrollTrigger = () => {
+    const scrollingDom = scrollingDomRef.current;
+
+    const sections = [...scrollingDomRef.current.children];
+
+    const scrollTL = gsap.timeline({
+      scrollTrigger: {
+        id: 'pc-trigger-scroll',
+        trigger: scrollerDomRef.current,
+        pin: true,
+        start: 'center center',
+        end: `+=${scrollingDom.offsetWidth * slides.length}`,
+        scrub: true,
+        anticipatePin: 1,
+        invalidateOnRefresh: true,
+        markers: isDebug,
+        onUpdate: (instance) => {
+          const progress = MathUtils.clamp(instance.progress, 0, 1);
+          setProgress(progress);
+        },
+      },
+    });
+
+    // const increment =
+    //   (scrollingDom.scrollWidth - scrollerDomRef.current.clientWidth) /
+    //   (sections.length - 1);
+
+    sections.forEach((section, index) => {
+      scrollTL.to(
+        sections,
+        {
+          // x: () => `-=${increment}px`,
+          x: () => `-=${window.innerWidth * 1}px`,
+          // x: () => `-=${window.innerWidth * 2}px`,
+          ease: 'none',
+        },
+        '+=0.3'
+      );
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    ScrollTrigger.getById('pc-trigger-scroll')?.kill();
+    gsap.registerPlugin(ScrollTrigger);
+    setUpScrollTrigger();
+    return () => {};
+  }, []);
+
+  return (
+    <section
+      ref={scrollerDomRef}
+      className={cx(
+        css`
+          position: relative;
+          width: 100%;
+          min-height: 100vh;
+          overflow: hidden;
+        `,
+        'viewport scroll'
+      )}
+    >
+      <div
+        className={css`
+          position: absolute;
+          overflow: hidden;
+          top: 0;
+        `}
+      >
+        <div
+          ref={scrollingDomRef}
+          className={cx(
+            css`
+              width: ${slides.length * 100}vw;
+              /* width: ${slides.length * 200}vw; */
+              height: 100%;
+              display: flex;
+              .slide {
+                flex-shrink: 0;
+                align-items: center;
+                justify-content: center;
+                display: flex;
+                min-height: 100vh;
+                width: 100vw;
+                /* width: 200vw; */
+                /* border: 1px solid blue; */
+                position: relative;
+              }
+            `,
+            `bg-gray-100`
+          )}
+        >
+          {slides.map((slide, index) => {
+            return (
+              <div className="slide" key={index}>
+                {slide.component({
+                  progress,
+                  startProgress: slide.startProgress,
+                  endProgress: slide.endProgress,
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export {HorizontalScrollContent};
--- react-gsap-scrolltrigger-horizontal-scroll-pin-vite-app/src/main.jsx
+++ react-gsap-scrolltrigger-horizontal-scroll-pin-vite-app/src/main.jsx
@@ -1,139 +1,48 @@
 import {createRoot} from 'react-dom/client';
 import {css, cx} from '@emotion/css';
-import {Spacer} from './components/Spacer';
-
-import gsap from 'gsap';
-import {ScrollToPlugin} from 'gsap/ScrollToPlugin';
-import {ScrollTrigger} from 'gsap/ScrollTrigger';
-
+import Lenis from '@studio-freight/lenis';
+import {useEffect, useRef, useMemo} from 'react';
+import {HorizontalScrollContent} from './components/HorizontalScrollContent';
 import '@fontsource/inter';
 import './styles/index.css';
 import './styles/index.scss';
-import {useEffect, useRef} from 'react';
-import {useMemo} from 'react';
-
-import {MathUtils} from 'three';
-import {useState} from 'react';
-import {useDebouncedCallback} from 'use-debounce';
-import {BebopLottie} from './components/BebopLottie';
 
 const App = () => {
-  const scrollerDomRef = useRef(null);
-  const scrollingDomRef = useRef(null);
-  const [progress, setProgress] = useState(0);
+  // const reqId = useRef(null);
 
-  const slides = useMemo(() => {
-    return [
-      {
-        component: () => {
-          return (
-            <div className="flex items-center">
-              <h2 className="text-3xl">Slide 1</h2>
-            </div>
-          );
-        },
-      },
-      {
-        startProgress: 0.25,
-        endProgress: 0.5,
-        component: ({progress, startProgress, endProgress}) => {
-          return (
-            <div className="flex items-center flex-col justify-center">
-              <h2 className="text-3xl">Slide 2</h2>
-              <BebopLottie
-                progress={progress}
-                startProgress={startProgress}
-                endProgress={endProgress}
-              />
-            </div>
-          );
-        },
-      },
-      {
-        component: () => {
-          return (
-            <div className="flex items-center">
-              <h2 className="text-3xl">Slide 3</h2>
-            </div>
-          );
-        },
-      },
-      {
-        startProgress: 0.75,
-        endProgress: 1.0,
-        component: ({progress, startProgress, endProgress}) => {
-          return (
-            <div className="flex items-center flex-col justify-center">
-              <h2 className="text-3xl">Slide 4</h2>
-              <BebopLottie
-                progress={progress}
-                startProgress={startProgress}
-                endProgress={endProgress}
-              />
-            </div>
-          );
-        },
-      },
-    ];
-  }, []);
+  // const lenis = useMemo(() => {
+  //   // Smooth scrolling initialization (using Lenis https://github.com/studio-freight/lenis)
+  //   if (window.matchMedia('(max-width: 768px)').matches) {
+  //     return new Lenis({
+  //       lerp: 0.05,
+  //       smooth: true,
+  //     });
+  //     // return null;
+  //   } else {
+  //     return new Lenis({
+  //       lerp: 0.05,
+  //       smooth: true,
+  //     });
+  //   }
+  // }, []);
 
-  const handleResize = useDebouncedCallback((e) => {
-    ScrollTrigger.getById('pc-trigger-scroll')?.kill();
-    setUpScrollTrigger();
-  }, 500);
+  // useEffect(() => {
+  //   if (lenis) {
+  //     (() => {
+  //       const animate = (time) => {
+  //         lenis.raf();
+  //         reqId.current = window.requestAnimationFrame(animate);
+  //       };
+  //       animate();
+  //     })();
+  //   }
+  //   return () => {
+  //     if (reqId.current) {
+  //       window.cancelAnimationFrame(reqId.current);
+  //     }
+  //   };
+  // }, []);
 
-  const setUpScrollTrigger = () => {
-    const scrollingDom = scrollingDomRef.current;
-
-    const sections = [...scrollingDomRef.current.children];
-
-    const scrollTL = gsap.timeline({
-      scrollTrigger: {
-        id: 'pc-trigger-scroll',
-        trigger: scrollerDomRef.current,
-        pin: true,
-        start: 'center center',
-        end: `+=${scrollingDom.offsetWidth * slides.length}`,
-        scrub: true,
-        anticipatePin: 1,
-        invalidateOnRefresh: true,
-        markers: true,
-        onUpdate: (instance) => {
-          const progress = MathUtils.clamp(instance.progress, 0, 1);
-          setProgress(progress);
-        },
-      },
-    });
-
-    const increment =
-      (scrollingDom.scrollWidth - scrollerDomRef.current.clientWidth) /
-      (sections.length - 1);
-
-    sections.forEach((section, index) => {
-      scrollTL.to(
-        sections,
-        {
-          x: () => `-=${increment}px`,
-          ease: 'none',
-        },
-        '+=0.3'
-      );
-    });
-  };
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    ScrollTrigger.getById('pc-trigger-scroll')?.kill();
-    gsap.registerPlugin(ScrollTrigger);
-    setUpScrollTrigger();
-    return () => {};
-  }, []);
   return (
     <div>
       <section className="min-h-screen w-full flex items-center justify-center text-3xl">
@@ -143,46 +52,7 @@ const App = () => {
         Scene2
       </section>
 
-      <section
-        ref={scrollerDomRef}
-        className={cx(
-          css`
-            min-height: 100vh;
-            overflow: hidden;
-          `,
-          'viewport scroll'
-        )}
-      >
-        <div
-          ref={scrollingDomRef}
-          className={cx(
-            css`
-              width: ${slides.length * 100}%;
-              height: 100%;
-              display: flex;
-              .slide {
-                align-items: center;
-                justify-content: center;
-                display: inline-flex;
-                width: 100vw;
-                border: 1px solid blue;
-              }
-            `
-          )}
-        >
-          {slides.map((slide, index) => {
-            return (
-              <div className="slide" key={index}>
-                {slide.component({
-                  progress,
-                  startProgress: slide.startProgress,
-                  endProgress: slide.endProgress,
-                })}
-              </div>
-            );
-          })}
-        </div>
-      </section>
+      <HorizontalScrollContent />
 
       <section className="min-h-screen w-full flex items-center justify-center text-3xl">
         Scene3
